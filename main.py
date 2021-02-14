92ef642076930816c98107882251b734b3a60a45
stefutz101 <60782567+stefutz101@users.noreply.github.com>
2021-02-14 16:50:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -161,9 +161,3 @@ def cron():
     cnx.close()
 
     return "Done"
-
-if __name__ == "__main__":
-    # Used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host="localhost", port=8080, debug=True)
