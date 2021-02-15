36eaea74909f884e756b0ad43e247108511ee3e1
stefutz101 <60782567+stefutz101@users.noreply.github.com>
2021-02-15 20:45:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def index():
 @app.route("/cron", methods=["GET"])
 def cron():
     logging.warning(request.headers)
-    
-    if request.headers.get('X-Appengine-Cron') != True:
-        return '401 Unauthorized'
+
+    # if request.headers.get('X-Appengine-Cron') != True:
+    #    return '401 Unauthorized'
 
     nltk.download('vader_lexicon')
 
